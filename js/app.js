77116a938bb6734e00f3e96fbf4741497aa2d3a3
Alan White <alan@whitemail.net>
2017-03-23 14:35:38 +0000

    hero outline

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).foundation()
+
+window.addEventListener("load", function(){
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#252e39"
+      },
+      "button": {
+        "background": "transparent",
+        "text": "#14a7d0",
+        "border": "#14a7d0"
+      }
+    },
+    "position": "bottom-right",
+    "content": {
+      "message": "The crazy EU cookie law says I have to show you this. This website uses cookies - if you don't like that then please leave the site."
+    }
+  })
+});
