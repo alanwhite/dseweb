a42f3ecfd3e20040ab4314fefd110f01ec34f69f
Alan White <alan@whitemail.net>
2017-04-04 08:37:37 +0100

    alignment of license buttons

--- app/js/app.js
+++ app/js/app.js
@@ -72,65 +72,6 @@ var authHook = {
   }
 }
 
-// licGet = function() {
-//   console.log("set up the ajax call");
-//   // populate the correct div with license detail
-//   var profile = JSON.parse(localStorage.getItem('profile'));
-//   var token = profile.sub.substring(7);
-//
-//   $.ajax({
-//     url: 'https://sja5mclnha.execute-api.eu-west-1.amazonaws.com/dev/licenses/58dc1f657e89a40270feef49',
-//
-//     beforeSend: function(request) {
-//       request.setRequestHeader("Authorization", "Bearer "+localStorage.getItem('idToken'));
-//     },
-//     data: { "apiVersion" : 1 },
-//
-//     success: function(msg) {
-//       console.log("GET completed: "+msg);
-//       if ( msg.apiVersion ) {
-//         if ( msg.apiVersion == 1 ) {
-//           var licThanks = document.getElementById("license-thanks");
-//           var numLicenses = msg.licenses.length;
-//           if ( numLicenses == 0 ) {
-//             licThanks.textContent = "Please consider financially supporting Drum Score Editor "+
-//               "by purchasing a license";
-//           } else {
-//             populateLicenseTable(msg.licenses);
-//           }
-//         } else {
-//           console.log("unexpected API version "+msg.apiVersion+" received");
-//           alert("Failed to format license data from server");
-//         }
-//       } else {
-//         console.log("malformed API response: no apiVersion returned");
-//         alert("Failed to retrieve license data from server");
-//       }
-//     }
-//
-//   });
-// }
-//
-// populateLicenseTable = function(licenses) {
-//   var licenseTable = document.getElementById("license-table");
-//   var licBody = licenseTable.getElementsByTagName('tbody')[0];
-//   for (var i in licenses ) {
-//     console.log(licenses[i].token);
-//     var row = licBody.insertRow(i);
-//     var cell1 = row.insertCell(0);
-//     cell1.innerHTML = licenses[i].token;
-//   }
-// }
-//
-// clearLicenseTable = function() {
-//   var licenseTable = document.getElementById("license-table");
-//   var licBody = licenseTable.getElementsByTagName('tbody')[0];
-//   var licRows = licBody.length;
-//   for ( var i in licBody ) {
-//     licBody.deleteRow(i);
-//   }
-// }
-
 UserManagement.init(authHook);
 
 btnAuth.onclick = function() {
@@ -138,8 +79,6 @@ btnAuth.onclick = function() {
     UserManagement.logout();
   } else {
     UserManagement.login();
-    // localStorage.setItem('idToken','fred');
-    // authHook.loginSuccess();
   }
 }
 
