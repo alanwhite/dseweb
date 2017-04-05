758eb2ffac4dcb1bf665ee90b9bc9ab274c5a6ca
Alan White <alan@whitemail.net>
2017-04-05 17:42:14 +0100

    build fix for ordering of minified assets

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ var gulp = require('gulp'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
     livereload = require('gulp-livereload'),
-    del = require('del');
+    del = require('del'),
+    order = require('gulp-order'),
     htmlreplace = require('gulp-html-replace');
 
 var input = 'src';
@@ -21,10 +22,14 @@ gulp.task('default', ['clean'], function() {
 
 gulp.task('styles', function() {
   return gulp.src(input+'/css/*.css')
+    .pipe(order([
+      "foundation.css",
+      "app.css"
+    ]))
     .pipe(rename({suffix: '.min'}))
     .pipe(cssnano())
     .pipe(concat('styles.min.css'))
-    .pipe(gulp.dest(output+'/assets/css'))
+    .pipe(gulp.dest(output+'/css'))
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
@@ -33,31 +38,39 @@ gulp.task('scripts', function() {
     // .pipe(jshint('.jshintrc'))
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
+    .pipe(order([
+      "vendor/jquery.js",
+      "vendor/*.js",
+      "user-management.js",
+      "license-mgmt.js",
+      "app.js",
+      "*.js"
+    ]))
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('build/assets/js'))
+    .pipe(gulp.dest('build/js'))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
-    .pipe(gulp.dest('build/assets/js'))
+    .pipe(gulp.dest('build/js'))
     .pipe(notify({ message: 'Scripts task complete' }));
 });
 
 gulp.task('images', function() {
   return gulp.src('src/images/**/*')
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('build/assets/img'))
+    .pipe(gulp.dest('build/images'))
     .pipe(notify({ message: 'Images task complete' }));
 });
 
 gulp.task('html', function () {
     return gulp.src('src/index.html')
         .pipe(htmlreplace({
-          css: 'assets/css/styles.min.css',
-          js: 'assets/js/main.min.js'
+          css: 'css/styles.min.css',
+          js: 'js/main.min.js'
         }))
         .pipe(gulp.dest('build'))
         .pipe(notify({ message: 'html task complete' }));
 });
 
 gulp.task('clean', function() {
-  return del(['build/assets/css', 'build/assets/js', 'build/assets/img']);
+  return del(['build/css', 'build/js', 'build/img']);
 });
