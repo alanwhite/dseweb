d648236c3996156ce7ea68afbc7801e4b65e6b15
Alan White <alan@whitemail.net>
2017-04-21 14:57:53 +0100

    fix prod URL names

--- src/js/environment.js
+++ src/js/environment.js
@@ -10,9 +10,9 @@ const dev_PAYPAL_ACCOUNT = 'alan-facilitator@whiteware.org';
 
 const prod_AUTH0_CLIENT_ID = 'k3CauTTm89JUxldE3Md43qDwDtSZksH5';
 const prod_AUTH0_DOMAIN = 'drumscore.eu.auth0.com';
-const prod_DSE_GET_LICENSE_URL = 'https://sja5mclnha.execute-api.eu-west-1.amazonaws.com/prod/licenses/';
+const prod_DSE_GET_LICENSE_URL = 'https://k1bs3h70j0.execute-api.eu-west-1.amazonaws.com/prod/licenses/';
 const prod_PAYPAL_BUY_NOW_URL = 'https://www.paypal.com/cgi-bin/webscr';
-const prod_PAYPAL_NOTIFY_URL = 'https://sja5mclnha.execute-api.eu-west-1.amazonaws.com/prod/payments/paypal';
+const prod_PAYPAL_NOTIFY_URL = 'https://k1bs3h70j0.execute-api.eu-west-1.amazonaws.com/prod/payments/paypal';
 const prod_PAYPAL_ACCOUNT = 'alan@whiteware.org';
 
 // below values edited by build process, searches for "_STAGE = dev_" and replaces with prod as needed
